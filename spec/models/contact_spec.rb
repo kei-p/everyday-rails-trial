f61acfe4fcca3cbe6944191e76aa2db87a701727
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2016-05-01 13:46:48 +0900

    Arrange spec with describe and context

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -56,41 +56,36 @@ describe Contact do
     expect(contact.name).to eq('John Doe')
   end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'T',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjhonson@example.com'
-    )
-    expect(Contact.by_letter("J")).to eq([johnson, jones])
-  end
 
-  it "omits results that do not match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'T',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjhonson@example.com'
-    )
-    expect(Contact.by_letter("J")).not_to include(smith)
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'jsmith@example.com'
+      )
+      @jones = Contact.create(
+        firstname: 'T',
+        lastname: 'Jones',
+        email: 'tjones@example.com'
+      )
+      @johnson = Contact.create(
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjhonson@example.com'
+      )
+    end
+
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq([@johnson, @jones])
+      end
+    end
+
+    context "non-matching letters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter("J")).not_to include(@smith)
+      end
+    end
   end
 end
