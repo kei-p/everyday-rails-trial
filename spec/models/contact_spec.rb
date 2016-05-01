03ac2a5104b5d696d653f6b9b272172c37307201
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2016-05-01 13:39:13 +0900

    Add Contact.by_letter spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -47,5 +47,50 @@ describe Contact do
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
-  it 'returns a contact\'s full name as a string'
+  it 'returns a contact\'s full name as a string' do
+    contact = Contact.new(
+      firstname: 'John',
+      lastname: 'Doe'
+    )
+
+    expect(contact.name).to eq('John Doe')
+  end
+
+  it "returns a sorted array of results that match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'T',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjhonson@example.com'
+    )
+    expect(Contact.by_letter("J")).to eq([johnson, jones])
+  end
+
+  it "omits results that do not match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'T',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjhonson@example.com'
+    )
+    expect(Contact.by_letter("J")).not_to include(smith)
+  end
 end
